4ebcbb5d801eb49a8b537d278bb19260eaec121b
TANIGAWA Takashi <mihailjp@gmail.com>
2012-01-20 23:57:39 +0900

    ファイル分割

---
+++ hspcksum.h
@@ -0,0 +1,13 @@
+#ifndef __hspcksum_h
+#define __hspcksum_h
+
+#include <Windows.h>
+#include "hsp3plugin.h"
+
+extern char hashbuf[256];
+
+unsigned int leftrotate (unsigned int, int);
+static void *reffunc(int *, int);
+EXPORT void WINAPI hsp3cmdinit(HSP3TYPEINFO *);
+
+#endif
---
+++ sha1.h
@@ -0,0 +1,8 @@
+#ifndef __sha1_h
+#define __sha1_h
+
+#include "hsp3plugin.h"
+
+char* sha1calc(unsigned char *, size_t);
+
+#endif
