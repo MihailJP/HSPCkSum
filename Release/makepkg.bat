786261868d5892d04d2b59e7b255c561c3479a06
TANIGAWA Takashi <mihailjp@gmail.com>
2012-01-21 14:23:08 +0900

    サンプルがパッケージに入らないのを修正

--- Release/makepkg.bat
+++ Release/makepkg.bat
@@ -2,8 +2,6 @@ del /q hspcksum\common\*
 rmdir hspcksum\common
 del /q hspcksum\hsphelp\*
 rmdir hspcksum\hsphelp
-del /q hspcksum\sample\HSPCkSum\*
-rmdir hspcksum\sample\HSPCkSum
 del /q hspcksum\sample\*
 rmdir hspcksum\sample
 del /q hspcksum\*
@@ -14,11 +12,10 @@ mkdir hspcksum
 mkdir hspcksum\common
 mkdir hspcksum\hsphelp
 mkdir hspcksum\sample
-mkdir hspcksum\sample\HSPCkSum
 
 copy hspcksum.dll hspcksum
 copy common\* hspcksum\common
 copy hsphelp\* hspcksum\hsphelp
-copy sample\* hspcksum\sample
+copy sample\HSPCkSum\* hspcksum\sample\HSPCkSum
 rem copy readme.txt hspcksum
 "C:\Program Files\7-Zip\7z.exe" a -mx=9 hspcksum.zip hspcksum
